955b7032f663fbc0096981a32d815e9968458c2d
SphaeroX <sphaerox@gmail.com>
2025-06-15 10:09:23 +0200

    fixes

--- src_calibrate/main.cpp
+++ src_calibrate/main.cpp
@@ -3,11 +3,16 @@
 #include <SensirionI2cScd4x.h>
 #include "config.h"
 
+// Define the target CO2 concentration for calibration (in ppm)
+const uint16_t CALIBRATION_CO2_PPM = 424;
+
 SensirionI2cScd4x scd4x;
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
-    while (!Serial);
+    while (!Serial)
+        ;
     Serial.println("\n\nSCD41 Manual Calibration");
 
     Wire.begin();
@@ -17,22 +22,33 @@ void setup() {
     scd4x.reinit();
     scd4x.startPeriodicMeasurement();
 
-    Serial.println("Place the sensor in fresh air. Calibration will start in 60 seconds...");
+    // Inform the user to place the sensor in fresh air for calibration
+    Serial.print("Place the sensor in fresh air (");
+    Serial.print(CALIBRATION_CO2_PPM);
+    Serial.println(" ppm CO2). Calibration will start in 5 minutes...");
 }
 
-void loop() {
+void loop()
+{
     static bool calibrationDone = false;
     static unsigned long startMs = millis();
 
-    if (!calibrationDone && millis() - startMs >= 60000) {
-        Serial.println("Performing forced recalibration to 400 ppm...");
+    if (!calibrationDone && millis() - startMs >= 300000)
+    {
+        // Start forced recalibration using the defined CO2 value
+        Serial.print("Performing forced recalibration to ");
+        Serial.print(CALIBRATION_CO2_PPM);
+        Serial.println(" ppm...");
         scd4x.stopPeriodicMeasurement();
         uint16_t frcCorrection = 0;
-        int16_t err = scd4x.performForcedRecalibration(400, frcCorrection);
-        if (err == 0) {
+        int16_t err = scd4x.performForcedRecalibration(CALIBRATION_CO2_PPM, frcCorrection);
+        if (err == 0)
+        {
             Serial.printf("Calibration successful, correction: %u ppm\n", frcCorrection);
             scd4x.persistSettings();
-        } else {
+        }
+        else
+        {
             Serial.printf("Calibration failed, error: %d\n", err);
         }
         scd4x.startPeriodicMeasurement();
